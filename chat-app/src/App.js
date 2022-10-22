09c19adc59944201cbf58d049a51b00aa663e7f3
Darinka <dmal@itu.dk>
2022-10-22 15:23:22 -0500

    Navbar added

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Home from './pages/Home';
 import './styles.css' 
 
 function App() {
   return (
-    <Register/>
+   // <Register/>
+   <Home/>
   );
 }
 
