7e73ddd3adcbbaf2df439b3d4ad74806ef84a660
Darinka <dmal@itu.dk>
2022-11-06 15:21:49 +0100

    some temporary icons added

--- chat-app/src/components/SecondaryButton.jsx
+++ chat-app/src/components/SecondaryButton.jsx
@@ -1,13 +1,16 @@
-const SecondaryButton = ({ onClick, text, className }) => {
+import AddCommentIcon from '@mui/icons-material/AddComment';
+
+const SecondaryButton = ({ onClick, text, icon, className }) => {
 	return (
 	<button type="button" className={className} onClick={onClick}>
-		{text}
+		{text} {icon}
     </button>
   );
 }
 
 SecondaryButton.defaultProps = {
     text: 'Secondary Button',
+    icon: <AddCommentIcon/> ,
     className: "secondaryBtn"
 }
 
