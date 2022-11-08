c3c40cc29f69bf3a8050141c9445de8074f62b1c
Darinka <dmal@itu.dk>
2022-11-08 13:32:32 +0100

    fixed font

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -28,7 +28,8 @@ const router = createBrowserRouter([
               },
 ]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById("root")).render(
+        <React.StrictMode>
+                <RouterProvider router={router} />
+        </React.StrictMode>
+);
