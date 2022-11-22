4a5cbc350fedde8f13107151457722b8bc6c1d30
iga waclawska <iga.waclawska@gmail.com>
2022-11-22 10:03:49 +0100

    universal chat bubble

--- chat-app/src/containers/BubblesScrollable.jsx
+++ chat-app/src/containers/BubblesScrollable.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import RightChatBubble from "../components/RightChatBubble";
+import ChatBubble from "../components/ChatBubble";
 import "../styles.css";
 import { db } from "../firebase";
 import { doc, onSnapshot } from "firebase/firestore";
@@ -23,14 +23,8 @@ const BubblesScrollable = () => {
   return (
     <div className="bubbles-scrollable">
         {messages.map((m) => (
-        <RightChatBubble message={m} key={m.id} />
+        <ChatBubble message={m} key={m.id} />
       ))}
-      {/* <RightChatBubble></RightChatBubble> */}
-      {/* <RightChatBubble text="This is a looooooooooooooooooooooooong message"></RightChatBubble>
-      <RightChatBubble text="This is a letter: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></RightChatBubble>
-      <LeftChatBubble></LeftChatBubble>
-      <LeftChatBubble text="Nej"></LeftChatBubble>
-      <LeftChatBubble text="This is another letter: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></LeftChatBubble> */}
     </div>
   );
 };
