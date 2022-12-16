271d94eacbc17c40ccbb0a9ffbf1c651d8a1e51f
Darinka <dmal@itu.dk>
2022-12-16 13:29:18 +0100

    search tolowercase back

--- chat-app/src/containers/MessageModal.jsx
+++ chat-app/src/containers/MessageModal.jsx
@@ -22,10 +22,10 @@ const MessageModal = ({ show }) => {
 
   const handleSearch = async () => {
     if (username != "") {
+      let caseInsensitiveUsername = username.toLowerCase();
       const filter = query(
         collection(db, "users"),
-        where("displayName", "==", username)
-        // todo: check for names toLoweCase
+        where("displayNameLowerCase", "==", caseInsensitiveUsername)
       );
       try {
         const filteredDocs = await getDocs(filter);
