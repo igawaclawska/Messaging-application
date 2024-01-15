3cbf9abfcfb7a0c783637bf63ac8c2ddbf9d5b9b
iga waclawska <iga.waclawska@gmail.com>
2024-01-15 10:58:39 +0100

    adjust typography and style

--- chat-app/src/containers/ChatHeader.jsx
+++ chat-app/src/containers/ChatHeader.jsx
@@ -18,8 +18,8 @@ const ChatHeader = ({ onClick }) => {
           ></Button>{" "}
         </div>
         <span className="chat-header-title">
-          {data.group?.name != null ? <div>{data.group?.name}: </div> : ""}
-          <span className="chat-header-title-users">
+          {data.group?.name && <div>{data.group?.name}: </div>}
+          <span className={data.group?.name && "chat-header-title-users"}>
             {data.user1?.displayName} {data.user2?.displayName}{" "}
             {data.user3?.displayName} {data.user4?.displayName}{" "}
             {data.user5?.displayName}
