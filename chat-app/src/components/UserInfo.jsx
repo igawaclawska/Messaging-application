dbbcfd9610ff12e552dd284fb0338215ff090a57
iga waclawska <iga.waclawska@gmail.com>
2024-01-19 22:42:56 +0100

    redesign message modal

--- chat-app/src/components/UserInfo.jsx
+++ chat-app/src/components/UserInfo.jsx
@@ -1,18 +1,16 @@
-import './UserInfo.css'
+import "./UserInfo.css";
 
-const UserInfo = ({ onClick, value, displayName, uid, email, idx }) => {
+const UserInfo = ({ onClick, value, displayName, uid, email, idx, className }) => {
   return (
     <div
-      className={"user-info"}
+      className={className}
       value={value}
       onClick={onClick}
       uid={uid}
       idx={idx}
     >
       <p className="user-info-name">
-        <b>
-          {idx + 1}. {displayName}
-        </b>
+        <b>{displayName}</b>
       </p>
       <p className="user-info-email">{email}</p>
     </div>
