9f8cf05cdf57830078cecbc01c7b75cccb7278c3
iga waclawska <iga.waclawska@gmail.com>
2024-02-17 11:19:35 +0100

    adapt messageInput height to message

--- chat-app/src/components/MessageInput.jsx
+++ chat-app/src/components/MessageInput.jsx
@@ -1,9 +1,26 @@
 import "./MessageInput.css";
+import { useEffect, useRef } from "react";
 
 const MessageInput = ({ onKeyDown, value, type, onChange }) => {
+  const textAreaHeightRef = useRef(null);
+
+  useEffect(() => {
+    const adaptTextareaHeight = () => {
+      if (textAreaHeightRef.current) {
+        textAreaHeightRef.current.style.height = "auto";
+        textAreaHeightRef.current.style.height = `${textAreaHeightRef.current.scrollHeight}px`;
+      }
+    };
+
+    adaptTextareaHeight();
+    window.addEventListener("resize", adaptTextareaHeight);
+    return () => window.removeEventListener("resize", adaptTextareaHeight);
+  }, [value]);
+
   return (
     <>
       <textarea
+        ref={textAreaHeightRef}
         onKeyDown={onKeyDown}
         type={type}
         className={"messageInput"}
@@ -11,6 +28,7 @@ const MessageInput = ({ onKeyDown, value, type, onChange }) => {
         onChange={onChange}
         required={true}
         value={value}
+        id="scallable-text"
       />
     </>
   );
