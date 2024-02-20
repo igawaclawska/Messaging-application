5e20cfbecd739894a6016196e2ebcfc2ca908197
iga waclawska <iga.waclawska@gmail.com>
2024-02-20 18:42:08 +0100

    add emoji picker

--- chat-app/src/containers/SendMessage.jsx
+++ chat-app/src/containers/SendMessage.jsx
@@ -1,9 +1,5 @@
 import "./SendMessage.css";
 import React, { useContext, useState } from "react";
-import MessageButton from "../components/MessageButton";
-import SentimentSatisfiedAltRoundedIcon from "@mui/icons-material/SentimentSatisfiedAltRounded";
-import MessageInput from "../components/MessageInput";
-import EmojiPicker from "emoji-picker-react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatsContext } from "../context/ChatsContext";
 import {
@@ -15,21 +11,25 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { v4 as uuid } from "uuid";
+import MessageButton from "../components/MessageButton";
+import MessageInput from "../components/MessageInput";
+import EmojiPickerDropdown from "./EmojiPicker";
 
 const SendMessage = () => {
   const [text, setText] = useState("");
   const { userLogged } = useContext(AuthContext);
   const { data } = useContext(ChatsContext);
-  const [showPicker, setShowPicker] = useState(false);
 
   const handleKey = (e) => {
     if (text.trim() !== "" && e.keyCode === 13 && !e.shiftKey) {
       handleSend();
     }
   };
+  
   const handleSend = async () => {
     if (text.trim() !== "") {
       try {
+        setText("");
         await updateDoc(doc(db, "chats", data.chatsId), {
           messages: arrayUnion({
             id: uuid(),
@@ -40,35 +40,28 @@ const SendMessage = () => {
           }),
         });
 
-        if (data.user2 == null) {
-          await updateDoc(doc(db, "userChats", userLogged.uid), {
-            [data.chatsId + ".lastMessage"]: {
-              message: text,
-            },
-            [data.chatsId + ".date"]: {
-              date: serverTimestamp(),
-            },
-          });
+        await updateDoc(doc(db, "userChats", userLogged.uid), {
+          [data.chatsId + ".lastMessage"]: {
+            message: text,
+          },
+          [data.chatsId + ".date"]: {
+            date: serverTimestamp(),
+          },
+        });
 
-          await updateDoc(doc(db, "userChats", data.user1.uid), {
-            [data.chatsId + ".lastMessage"]: {
-              message: text,
-            },
-            [data.chatsId + ".date"]: {
-              date: serverTimestamp(),
-            },
-          });
-        }
+        await updateDoc(doc(db, "userChats", data.user1.uid), {
+          [data.chatsId + ".lastMessage"]: {
+            message: text,
+          },
+          [data.chatsId + ".date"]: {
+            date: serverTimestamp(),
+          },
+        });
+        setText("");
       } catch (err) {
         console.log(err);
       }
-      setText("");
     }
-  };
-
-  const onEmojiClick = (event) => {
-    setText((prevInput) => prevInput + " " + event.emoji + " ");
-    setShowPicker(false);
   };
 
   return (
@@ -78,15 +71,9 @@ const SendMessage = () => {
         onKeyDown={handleKey}
         onChange={(event) => setText(event.target.value)}
         value={text}
-      ></MessageInput>
+      />
+      <EmojiPickerDropdown setText={setText} />
       <MessageButton onClick={handleSend} />
-      <MessageButton onClick={() => setShowPicker((val) => !val)} />
-      {showPicker && (
-        <EmojiPicker
-          pickerStyle={{ width: "100%" }}
-          onEmojiClick={onEmojiClick}
-        />
-      )}
     </div>
   );
 };
